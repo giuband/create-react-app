15bd003465255d40e045688419d8586447d4f471
Andy Crow <andy.crow@travelperk.com>
2020-03-31 12:19:48 +0200

    Fix conflict

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -339,43 +339,33 @@ module.exports = function(webpackEnv) {
         {
           test: /\.(js|mjs|jsx|ts|tsx)$/,
           enforce: 'pre',
-          use: process.env.DISABLE_ESLINT === 'true'
-          ? undefined
-          : [
-              {
-                options: {
-                  cache: true,
-                  formatter: require.resolve('react-dev-utils/eslintFormatter'),
-                  eslintPath: require.resolve('eslint'),
-                  resolvePluginsRelativeTo: __dirname,
-                  // @remove-on-eject-begin
-                  ignore: isExtendingEslintConfig,
-                  baseConfig: (() => {
-                    // We allow overriding the config only if the env variable is set
-                    if (isExtendingEslintConfig) {
-                      const eslintCli = new eslint.CLIEngine();
-                      let eslintConfig;
-                      try {
-                        eslintConfig = eslintCli.getConfigForFile(
-                          paths.appIndexJs
-                        );
-                      } catch (e) {
-                        console.error(e);
-                        process.exit(1);
-                      }
-                      return eslintConfig;
-                    } else {
-                      return {
-                        extends: [require.resolve('eslint-config-react-app')],
-                      };
-                    }
-                  })(),
-                  useEslintrc: false,
-                  // @remove-on-eject-end
-                },
-                loader: require.resolve('eslint-loader'),
-              },
-            ],
+          use:
+            process.env.DISABLE_ESLINT === 'true'
+              ? undefined
+              : [
+                  {
+                    options: {
+                      cache: true,
+                      formatter: require.resolve(
+                        'react-dev-utils/eslintFormatter'
+                      ),
+                      eslintPath: require.resolve('eslint'),
+                      resolvePluginsRelativeTo: __dirname,
+                      // @remove-on-eject-begin
+                      ignore: isExtendingEslintConfig,
+                      baseConfig: isExtendingEslintConfig
+                        ? undefined
+                        : {
+                            extends: [
+                              require.resolve('eslint-config-react-app'),
+                            ],
+                          },
+                      useEslintrc: false,
+                      // @remove-on-eject-end
+                    },
+                    loader: require.resolve('eslint-loader'),
+                  },
+                ],
           include: paths.appSrc,
         },
         {
